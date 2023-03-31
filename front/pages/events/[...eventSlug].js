314a4a216939cdd39e1f6d3923521fa8cacde4dd
mim-mel <mfblade2014@gmail.com>
2023-03-31 16:26:46 +0900

    feat: 중첩된 동적 경로에 페이지 생성하기

---
+++ front/pages/events/[...eventSlug].js
@@ -0,0 +1,9 @@
+const FilteredEventsPage = () => {
+  return (
+    <div>
+      <h1>필터가 적용된 이벤트 페이지</h1>
+    </div>
+  );
+};
+
+export default FilteredEventsPage;
---
+++ front/pages/events/[eventId].js
@@ -0,0 +1,9 @@
+const EventDetailPage = () => {
+  return (
+    <div>
+      <h1>이벤트 디테일 페이지</h1>
+    </div>
+  );
+};
+
+export default EventDetailPage;

db53991906c3de0f56d00a32ced01368db5455b7
mim-mel <mfblade2014@gmail.com>
2023-04-02 18:46:10 +0900

    feat: button 컴포넌트 및 아이콘 svg추가

--- front/components/events/event-item.js
+++ front/components/events/event-item.js
@@ -1,4 +1,8 @@
-import Link from 'next/link';
+import Button from '../ui/button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
+import classes from './event-item.module.css';
 
 const EventItem = props => {
   const { title, image, date, location, id } = props;
@@ -14,21 +18,24 @@ const EventItem = props => {
   const exploreLink = `/events/${id}`;
 
   return (
-    <li>
+    <li className={classes.item}>
       <img src={'/' + image} alt={title} />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
         </div>
-        <div>
+        <div className={classes.date}>
           <time>{humanReadableDate}</time>
         </div>
-        <div>
+        <div className={classes.address}>
           <address>{formattedAddress}</address>
         </div>
       </div>
-      <div>
-        <Link href={exploreLink}>Explore Event</Link>
+      <div className={classes.actions}>
+        <Button link={exploreLink}>
+          <span>Explore Event</span>
+          <span className={classes.icon}><ArrowRightIcon/></span>
+        </Button>
       </div>
     </li>
   );

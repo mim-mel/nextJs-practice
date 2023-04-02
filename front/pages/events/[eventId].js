c08e8474220510872393049311fd2ed412fd26f2
mim-mel <mfblade2014@gmail.com>
2023-04-02 19:33:27 +0900

    feat: 디테일 페이지 생성

--- front/pages/events/[eventId].js
+++ front/pages/events/[eventId].js
@@ -1,8 +1,34 @@
+import { useRouter } from 'next/router';
+import { Fragment } from 'react';
+
+import EventSummary from '@/components/event-detail/event-summary';
+import { getEventById } from '@/dummy-data';
+import EventLogistics from '@/components/event-detail/event-logistics';
+import EventContent from '@/components/event-detail/event-content';
+
 const EventDetailPage = () => {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+
+  const event = getEventById(eventId);
+
+  // function getEventById(id) {
+  //   return DUMMY_EVENTS.find(event => event.id === id);
+  // }
+
+  if (!event) {
+    return <p>해당 페이지를 찾을 수 없습니다.</p>;
+  }
+
   return (
-    <div>
-      <h1>이벤트 디테일 페이지</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title}/>
+      <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title}/>
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 };
 
